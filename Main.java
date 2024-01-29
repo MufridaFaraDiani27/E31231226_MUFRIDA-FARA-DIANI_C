02c1a80d2da216612d5a84cefc9940ee6386338d
MufridaFaraDiani27 <e31231226@student.polije.ac.id>
2024-01-29 14:26:06 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        int a = 10;
+        int b = 10;
+        boolean c = true; 
+        
+        int hasilA = a++;
+        int hasilB = ++b;
+        boolean hasilC = !c;
+        
+        System.out.println(hasilA);
+        System.out.println(hasilB);
+        System.out.println(hasilC);
+    }
+    
+}
